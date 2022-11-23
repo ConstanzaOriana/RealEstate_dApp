c15c1250d1878c513391bbeb7ef5db5dd0348b9a
Constanza Oriana <cotyoriana95@gmail.com>
2022-11-23 15:12:40 -0300

    Modifiers && Functions

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 interface IERC721 {
@@ -10,50 +10,94 @@ interface IERC721 {
 }
 
 contract Escrow {
-    address payable public buyer;
+    address public nftAddress;
     address payable public seller;
     address public inspector;
-    mapping(address => uint) TotalAmount;
 
-enum State{
-	await_payment, await_delivery, completeTransaction
-}
+	// modifier onlyInspector => verify for "Only inspector call this method"
+	modifier onlyInspector() {
+		require(msg.sender == inspector);
+		_;
+	}
 
-State public state;
-
-modifier instate(State expected_state) {
-	require(state == expected_state);
-	_;
-}
-
-modifier onlyBuyer(){
-        require(msg.sender == buyer || msg.sender == inspector);
+ 	// modifier onlyBuyer => verify for "Only buyer call this method"
+	modifier onlyBuyer(){
+        require(msg.sender == buyer);
         _;
     }
 
-modifier onlySeller(){
+	// modifier onlySeller => verify for "Only seller call this method"
+	modifier onlySeller(){
 	require(msg.sender == seller);
 	_;
-}
+	}  
+    
+    
+	mapping(uint256 => bool) isListed; 
+	mapping(uint256 => uint256) purchasePrice;
+	mapping(uint256 => uint256) escrowAmount;
+	mapping(address => uint256) buyer;
+	mapping(uint256 => uint256) buyerList;
+	mapping(uint256 => bool) inspectionPassed;
 
-constructor(address payable _buyer, address payable _sender) {
-	inspector = msg.sender;
-	buyer = _buyer;
-	seller = _sender;
-	state = State.await_payment;
-}
+    constructor(address _nftAddress, address payable _seller, address _inspector, address _buyer) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+    }
 
-function confirm_payment() onlyBuyer instate(State.await_payment) public payable {
-	state = State.await_delivery;	
-}
+    function list (uint256 _nftID, address _buyer, uint256 _purchasePrice, uint256 _escrowAmount) public payable onlySeller {
+        // Transfer NFT from seller to this contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
 
-function confirm_Delivery() onlyBuyer instate(State.await_delivery) public {
-	seller.transfer(address(this).balance);
-	state = State.completeTransaction;
-}
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyerList[_nftID] = _buyer;
+    }
 
-function ReturnPayment() onlySeller instate(State.await_delivery) public {
-	buyer.transfer(address(this).balance);
-}
+    // Put Under Contract (only buyer - payable escrow)   
+    function depositEarnest(uint256 _TokenID) onlyBuyer public payable {
+		uint256 _payedAmount = msg.value;
+		require(_payedAmount >= escrowAmount[_TokenID], "Not enough");
+	}
+
+    // Update Inspection Status (only inspector)
+    function updateInspectionStatus () public view onlyInspector returns (bool) {
+		require(isListed[_nftID] == true, "That NFT doesnt exist");
+		inspectionPassed[_nftID] == !inspectionPassed[_nftID];
+	}
+    
+
+    // Approve Sale
+    function approveSale  /*complete*/ (uint256 _nftID) public returns (bool){
+        //approval[_nftID][msg.sender] = true;
+		require(isListed[_nftID] == true, "The NFT is not listed");
+		require(inspectionPassed[_nftID]  == true, "The inspection hasnt been approved");
+    }
+
+    // Finalize Sale
+	function finalizeSale() public onlyInspector{
+	// -> Require inspection status 
+    // -> Require sale to be authorized
+    // -> Require funds to be correct amount
+    // -> Transfer NFT to buyer
+    // -> Transfer Funds to Seller
+	}
+    
+    // Cancel Sale (handle earnest deposit)
+    function cancelSale () /*complete*/ {
+	// -> if inspection status is not approved, then refund, otherwise send to seller
+	}
+
+    //implement a special receive function in order to receive funds and increase the balance
+    function receive() external payable {
+
+	}
+
+    //function getBalance to check the current balance
+    function getBalance() /*complete*/ public view returns (uint256) {        
+		return address(this).balance;
+	}
 
 }
